b418734ae980abc6d0664eae91246b5967856b35
GokuHashira <gokulsenpai03@gmail.com>
2025-06-04 16:34:11 +0530

    Added loggers

--- ResponseAnalyzer/src/ResponseAnalyzer.py
+++ ResponseAnalyzer/src/ResponseAnalyzer.py
@@ -1,14 +1,26 @@
 import os
+import json
+from strategies import (
+    language_coverage,
+    language_coverage_similarity,
+    llm_as_judge,
+    text_similarity,
+    toxicity_model_result,
+    grammarcheck,
+    lexical_diversity,
+    bleu_score_metric,
+    meteor_metric,
+    rouge_score_metric)
 
+with open('Data/DataPoints.json', 'r') as file:
+    datapoints = json.load(file)
 
-with open('Data/metric_operation.json', 'r') as file:
-    metrics = json.load(file)
+with open('Data/plans.json', 'r') as file:
+    plans = json.load(file)
 
-with open('Data/chat_execution.json', 'r') as file:
-    chat_exec = json.load(file)
+with open('Data/responses.json', 'r') as file:
+    responses = json.load(file)
 
-with open('Data/prompts.json', 'r') as file:
-    prompts = json.load(file)
+with open('Data/strategy_map.json', 'r') as file:
+    strat_map = json.load(file)
 
-with open('Data/chats.json', 'r') as file:
-    chats = json.load(file)
