db2ccc9a01d4b4fe0e8d737b252b67ea91fe3916
iamnithyaramesh <nithyasri.ramesh2912@gmail.com>
2025-06-06 15:21:29 +0530

    bugs fixed in TEM code

--- TestCaseExecutionManager/src/TestCaseExecutionManager.py
+++ TestCaseExecutionManager/src/TestCaseExecutionManager.py
@@ -81,7 +81,6 @@ elif args.application_type == "OPENUI":
 
 class TestCaseExecutionManager:
     def __init__(self, test_plan_file, test_plan_id, limit=None, **client_args):
-    def __init__(self, test_plan_file, test_plan_id, limit=None, **client_args):
         self.test_plan_file = test_plan_file
         self.test_plan_id = test_plan_id
         self.test_plan_id = test_plan_id
@@ -99,7 +98,7 @@ class TestCaseExecutionManager:
         else:
             self.client = None
 
-    def load_test_cases(self) -> List[Dict]:
+
     def load_test_cases(self) -> List[Dict]:
         try:
 
@@ -300,8 +299,7 @@ client = InterfaceManagerClient(**client_args)
 manager = TestCaseExecutionManager(
         test_plan_file=args.test_plan_file,
         test_plan_id=args.test_plan_id,
-        test_plan_id=args.test_plan_id,
-        limit=args.n,
+        limit=args.test_case_count,
         **client_args
     )
 
