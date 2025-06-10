415d084bb3c59f80dd9a531f514e71f09cfe9556
iamnithyaramesh <nithyasri.ramesh2912@gmail.com>
2025-06-10 14:56:23 +0530

    updated README.md

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd TestCaseExecutionManager\src
-python TestCaseExecutionManager.py --test_plan_id T1 --test_case_count 2
-cd ..
-cd ..
-python ResponseAnalyzer\src\ResponseAnalyzer.py
-pause
