abab3353e82bf6b5d8bdf480038f00a266c8a8db
Dr. Su Sa <sudarsun@gmail.com>
2025-06-18 16:58:03 +0530

    added the shortcuts for importing.

---
+++ ResponseAnalysis/lib/data/__init__.py
@@ -0,0 +1,7 @@
+from .test_plan import TestPlan
+from .test_case import TestCase
+from .response import Response
+from .evaluation_metric import EvaluationMetric
+from .domain import Domain
+from .language import Language
+from .prompt import Prompt
