00fa54815d3c36613cbf2f7e387c5adaca9fe669
Dr. Su Sa <sudarsun@gmail.com>
2025-06-18 17:00:45 +0530

    made TestCase a pydantic data class.

--- ResponseAnalysis/lib/data/test_case.py
+++ ResponseAnalysis/lib/data/test_case.py
@@ -1,17 +1,23 @@
 from .prompt import Prompt
 from .response import Response
 from typing import Optional
+from pydantic import BaseModel, Field
 
-class TestCase:
+class TestCase(BaseModel):
     """
     Represents a test case in the response analysis system.
     Attributes:
+        name (str): The name of the test case.
         prompt (Prompt): The prompt associated with the test case.
         response (Response): The response associated with the test case.
         kwargs (dict): Additional keyword arguments for future extensibility.
     """
+    name: str = Field(..., description="The name of the test case.")
+    prompt: Prompt = Field(..., description="The prompt for the test case.")
+    response: Optional[Response] = Field(None, description="The response for the test case.")
+    kwargs: dict = Field(default_factory=dict, description="Additional keyword arguments for future extensibility")
     
-    def __init__(self, prompt: Prompt, response: Optional[Response] = None, **kwargs):
+    def __init__(self, name:str, prompt: Prompt, response: Optional[Response] = None, **kwargs):
         """
         Initializes a TestCase instance.
 
@@ -20,8 +26,7 @@ class TestCase:
             response (Response): The response for the test case.
             kwargs: Additional keyword arguments for future extensibility.
         """
-        self.prompt = prompt
-        self.response = response
+        super().__init__(name =name, prompt = prompt, response=response, kwargs = kwargs)
 
     def evaluate(self) -> float:
         """
