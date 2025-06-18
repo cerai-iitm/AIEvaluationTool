29214420f155ac9edc6d3f4dfe826851408faf41
Dr. Su Sa <sudarsun@gmail.com>
2025-06-18 17:01:03 +0530

    minor amends

--- ResponseAnalysis/lib/orm/tester.py
+++ ResponseAnalysis/lib/orm/tester.py
@@ -5,7 +5,7 @@ import os
 # setup the relative import path for data module.
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from data.prompt import Prompt
+from data import Prompt, TestCase
 
 # __len__ __getitem__ __setitem__ __delitem__ __iter__ __contains__
 # __enter__ __exit__  contextual management methods for the DB class
@@ -17,15 +17,17 @@ db = DB()
 print("\n".join([repr(_) for _ in db.languages]))
 print(db.get_language_name(2))
 lang_english = db.get_language_id('english')
-domain_id = db.get_domain_id('education')
+domain_id = db.get_domain_id('agriculture')
 
-newPrompt = Prompt(system_prompt="You are an educational counselor. Provide guidance in a simple and clear manner.", 
-                   user_prompt="How can I improve my coding skills?",
+newPrompt = Prompt(system_prompt="Answer yes or no to the following question.", 
+                   user_prompt="Can cotton grow in red soil?",
                    domain_id=domain_id,
                    lang_id=lang_english)
 
 #prompt_id = db.add_prompt(newPrompt)
 #print(newPrompt, "added with id:", prompt_id)
 
-tc_id = db.create_testcase(testcase_name="tc1", prompt=newPrompt)
+#tc_id = db.create_testcase(testcase_name="tc1", prompt=newPrompt)
+tc = TestCase(name="tc1", prompt=newPrompt, response=None)
+tc_id = db.create_testcase(tc)
 print(f"Test case created with ID: {tc_id}")
