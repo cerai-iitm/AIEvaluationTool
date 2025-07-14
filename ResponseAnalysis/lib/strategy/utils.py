1ee8c7df688c4fcf31a640d27e1f8f9604a8cfc2
iamnithyaramesh <nithyasri.ramesh2912@gmail.com>
2025-07-14 11:53:35 +0530

    remove the unused mae code in utils.py

--- ResponseAnalysis/lib/strategy/utils.py
+++ ResponseAnalysis/lib/strategy/utils.py
@@ -380,17 +380,3 @@ class BARTScorer:
 
         print(self.score(src_list, tgt_list, batch_size))
 
-'''def calculate_mae_score( predicted_scores: list, ground_truth_key:list) -> float:
-    """
-    Calculate Mean Absolute Error (MAE) between predicted and actual scores.
-    Args:
-        prompts: List of prompt dicts containing ground truth values
-        predicted_scores: List of predicted float scores
-        ground_truth_key: Key in prompt dict for ground truth value
-    Returns:
-        MAE score as float
-    """
-    pred_array = np.array(predicted_scores)
-    actual_array = np.array(ground_truth_key)
-    mae = np.mean(np.abs(pred_array - actual_array))
-    return float(mae)'''
